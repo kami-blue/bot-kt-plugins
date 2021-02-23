163a404cd77f60eee583cbc1719e05912f3b8b50
lv <~@l1v.in>
2021-02-23 13:28:37 -0500

    [fix] ExportRoles bugs and formatting

--- kami-blue-plugin/src/org/kamiblue/botkt/plugins/main/commands/ExportRolesCommand.kt
+++ kami-blue-plugin/src/org/kamiblue/botkt/plugins/main/commands/ExportRolesCommand.kt
@@ -1,10 +1,15 @@
 package org.kamiblue.botkt.plugins.main.commands
 
+import net.ayataka.kordis.entity.message.MessageBuilder
 import org.kamiblue.botkt.PermissionTypes
 import org.kamiblue.botkt.command.BotCommand
 import org.kamiblue.botkt.command.Category
 import org.kamiblue.botkt.command.options.HasPermission
 import org.kamiblue.botkt.command.options.ServerOnly
+import org.kamiblue.botkt.utils.Colors
+import org.kamiblue.botkt.utils.upload
+import org.kamiblue.commons.utils.MathUtils
+import java.io.File
 
 object ExportRolesCommand : BotCommand(
     name = "ers",
@@ -13,17 +18,38 @@ object ExportRolesCommand : BotCommand(
 ) {
     init {
         execute(HasPermission.get(PermissionTypes.COUNCIL_MEMBER), ServerOnly) {
-            val members = server!!.members.filter { it.roles.isNotEmpty() }
+            val server = server!!
+            val members = server.members.filter { it.roles.size > 1 }
 
-            channel.send("```csv\n" +
-                    members.joinToString("\n") {
-                        "${it.id}," +
-                                it.roles.joinToString(",") { role ->
-                                    "${it.id}:${it.name}"
-                                }
-                    } +
-                    "\n```"
-            )
+            val formatted =
+                members.joinToString("\n") {
+                    "${it.id},${it.tag}," + it.roles
+                        .filter { role -> !role.isEveryone }
+                        .joinToString(",") { role ->
+                            "${role.id}:${role.name}"
+                        }
+                }
+
+            val embed = MessageBuilder().apply {
+                embed {
+                    field("Members with roles", "${members.size}")
+                    field("Total members", "${server.members.size}")
+                    field(
+                        "% of members with roles",
+                        "${MathUtils.round((members.size / server.members.size) * 100.0, 3)}%"
+                    )
+                    color = Colors.PRIMARY.color
+                }
+            }.build()
+
+            val file = File("cache/roles-cache-${server.id}.csv")
+            file.bufferedWriter().use {
+                it.write(formatted)
+            }
+
+            channel.upload(file, embed = embed)
+
+            file.delete()
         }
     }
 }
