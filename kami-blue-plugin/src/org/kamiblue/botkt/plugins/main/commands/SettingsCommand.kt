97df7db66887f941d36a898b7eebbc030127c6a7
d1snin <d1snin@aol.com>
2021-03-18 16:37:35 +0300

    fix: requested changes

--- kami-blue-plugin/src/org/kamiblue/botkt/plugins/main/commands/SettingsCommand.kt
+++ kami-blue-plugin/src/org/kamiblue/botkt/plugins/main/commands/SettingsCommand.kt
@@ -4,16 +4,16 @@ import org.kamiblue.botkt.command.BotCommand
 import org.kamiblue.botkt.command.Category
 import org.kamiblue.botkt.utils.normal
 
-object SafeCommand : BotCommand(
-        name = "safe",
-        description = "Is KAMI Blue safe?",
+object SettingsCommand : BotCommand(
+        name = "settings",
+        description = "CrystalAura settings",
         category = Category.INFO
 ) {
     init {
         execute {
             channel.normal(
-                    title = "Settings for KAMI Blue",
-                    description = "The default settings are already good, just adjust the settings to suit your playstyle and ping.")
+                    title = "CrystalAura settings for KAMI Blue",
+                    description = "The settings for the CrystalAura by default are configured as well as possible, set them up in accordance with your ping, playstile, etc")
         }
     }
 }
