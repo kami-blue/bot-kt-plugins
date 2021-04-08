83a288ccbc5b8d7ad256a336323f426dd4edda74
lv <~@l1v.in>
2021-04-08 12:57:23 -0400

    fix: LearnCommand links

--- kami-blue-plugin/src/org/kamiblue/botkt/plugins/main/commands/LearnCommand.kt
+++ kami-blue-plugin/src/org/kamiblue/botkt/plugins/main/commands/LearnCommand.kt
@@ -17,12 +17,11 @@ object LearnCommand : BotCommand(
                     "In order to make a Minecraft mod, you should at least know the basics of Java or Kotlin first, these resources can help you learn the fundamentals of Java, Gradle, Forge and Kotlin.\n\n" +
                     "If you don't take the time to learn the languages you're working on, you **will** get tripped up by the smallest problem that could have been resolved if you actually learned. " +
                     "**No one will help you if you don't make an effort to learn first.**\n\n" +
-                    "https://docs.gradle.org/current/samples/sample_building_java_applications.html/\n" +
-                    "https://kotlinlang.org/docs/tutorials/\n" +
-                    "https://minecraft.gamepedia.com/Tutorials/Creating_Forge_mods/\n" +
-                    "https://www.codecademy.com/learn/learn-java/\n" +
-                    "https://fabricmc.net/wiki/\n" +
-                    "https://github.com/SpongePowered/Mixin/wiki/"
+                    "<https://docs.gradle.org/current/samples/sample_building_java_applications.html>\n" +
+                    "<https://kotlinlang.org/docs/home.html>\n" +
+                    "<https://www.codecademy.com/learn/learn-java/>\n" +
+                    "<https://fabricmc.net/wiki/>\n" +
+                    "<https://github.com/SpongePowered/Mixin/wiki/>"
             )
         }
     }
