c2cdcb7f60b6f3bc88e7e47e6fb7801bf3d00316
sourTaste000 <47074495+sourTaste000@users.noreply.github.com>
2021-04-12 11:03:34 -0700

    delete unused imports

--- kami-blue-plugin/src/org/kamiblue/botkt/plugins/main/managers/BottomDeleteManager.kt
+++ kami-blue-plugin/src/org/kamiblue/botkt/plugins/main/managers/BottomDeleteManager.kt
@@ -1,13 +1,7 @@
 package org.kamiblue.botkt.plugins.main.managers
 
 import net.ayataka.kordis.event.events.message.MessageReceiveEvent
-import org.kamiblue.botkt.ConfigType
-import org.kamiblue.botkt.PermissionConfig
-import org.kamiblue.botkt.PermissionTypes
-import org.kamiblue.botkt.Permissions.hasPermission
 import org.kamiblue.botkt.manager.Manager
-import org.kamiblue.botkt.manager.managers.ConfigManager
-import org.kamiblue.botkt.utils.Colors
 import org.kamiblue.botkt.utils.tryDelete
 import org.kamiblue.event.listener.asyncListener
 
