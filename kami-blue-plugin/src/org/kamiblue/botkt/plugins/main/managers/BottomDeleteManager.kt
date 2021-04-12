fc0a47648a65cddaf322f684e5d25705baebf23c
sourTaste000 <47074495+sourTaste000@users.noreply.github.com>
2021-04-12 11:06:40 -0700

    whoops

--- kami-blue-plugin/src/org/kamiblue/botkt/plugins/main/managers/BottomDeleteManager.kt
+++ kami-blue-plugin/src/org/kamiblue/botkt/plugins/main/managers/BottomDeleteManager.kt
@@ -5,7 +5,7 @@ import org.kamiblue.botkt.manager.Manager
 import org.kamiblue.botkt.utils.tryDelete
 import org.kamiblue.event.listener.asyncListener
 
-object PingDeleteManager : Manager {
+object BottomDeleteManager : Manager {
     init {
         asyncListener<MessageReceiveEvent> { event ->
             if (event.message.contains("🥺") && event.message.contains("💖") && event.message.contains("✨")) {
